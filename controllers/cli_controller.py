fe5cb576b5f7e9143402b7c6e7d6dca82bd0cf56
zMyrddin <janmarvin.consul@gmail.com>
2023-06-24 12:52:24 +1000

    Seed comment table

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from init import db, bcrypt
 from models.user import User
 from models.card import Card
+from models.comment import Comment
 from datetime import date
 
 db_commands = Blueprint('db', __name__)
@@ -69,6 +70,31 @@ def seed_db():
     ]
 
     db.session.add_all(cards)
+
+    comments = [
+        Comment(
+            message="Comment 1",
+            user=users[0],
+            card=cards[0]
+        ),
+        Comment(
+            message="Comment 2",
+            user=users[1],
+            card=cards[2]
+        ),
+        Comment(
+            message="Comment 3",
+            user=users[1],
+            card=cards[3]
+        ),
+        Comment(
+            message="Comment 4",
+            user=users[0],
+            card=cards[3]
+        )
+    ]
+
+    db.session.add_all(comments)
 
     db.session.commit()
 
