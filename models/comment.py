2d73cc1d3f4b129d9fe7b68024a02700d6b0f1ab
zMyrddin <janmarvin.consul@gmail.com>
2023-06-29 20:55:10 +1000

    Comment Controllers

--- models/comment.py
+++ models/comment.py
@@ -1,16 +1,17 @@
 from init import db, ma
 from marshmallow import fields
 
+ # {id: 1, "message": "Comment 1", "user": {id: 1, name: "User 1"}, "card": {id: 1, title: "Card 1"}}
 class Comment(db.Model):
-    __tablename__ = "comments"
+    __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.Text)
-    
+
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     card_id = db.Column(db.Integer, db.ForeignKey('cards.id'), nullable=False)
 
-    user = db.relationship('User', back_populates='comments')
+    user = db.relationship('User', back_populates='comments') # {id: 1, name: "User 1"}
     card = db.relationship('Card', back_populates='comments')
 
 class CommentSchema(ma.Schema):
