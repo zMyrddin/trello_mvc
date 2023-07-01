8cbbe6b2a315acccf5b257f21e217f966e6697ea
zMyrddin <janmarvin.consul@gmail.com>
2023-07-01 12:01:19 +1000

    Flask Validation for Card Schema

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from init import db, ma, bcrypt, jwt
 from controllers.cli_controller import db_commands
 from controllers.auth_controller import auth_bp
 from controllers.card_controller import cards_bp
+from marshmallow import ValidationError
 
 def create_app():
     app = Flask(__name__)
@@ -14,6 +15,10 @@ def create_app():
 
     app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URL")
     app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
+
+    @app.errorhandler(ValidationError)
+    def validation_error(err):
+        return {'error': err.messages}, 400
 
     db.init_app(app)
     ma.init_app(app)
--- models/card.py
+++ models/card.py
@@ -1,5 +1,10 @@
 from init import db, ma
-from marshmallow import fields
+from marshmallow import fields, validates
+from marshmallow.validate import Length, And, Regexp, OneOf
+from marshmallow.exceptions import ValidationError
+
+VALID_STATUSES = ('To Do', 'Done', 'Ongoing', 'Testing', 'Deployed')
+VALID_PRIORITIES = ('Low', 'Medium', 'High', 'Urgent')
 
 class Card(db.Model):
     __tablename__ = "cards"
@@ -19,6 +24,24 @@ class Card(db.Model):
 class CardSchema(ma.Schema):
     user = fields.Nested('UserSchema', only=['name', 'email'])
     comments = fields.List(fields.Nested('CommentSchema'), exclude=['card'])
+
+    title = fields.String(required=True, validate=And(
+        Length(min=2, error='Title must be at least 2 characters long'),
+        Regexp('^[a-zA-Z0-9 ]+$', error='Only letters, spaces and numbers are allowed')
+    ))
+
+    status = fields.String(validate=OneOf(VALID_STATUSES))
+
+    priority = fields.String(validate=OneOf(VALID_PRIORITIES))
+
+    @validates('status')
+    def validate_status(self, value):
+        if value == VALID_STATUSES[2]:
+            stmt = db.select(db.func.count()).select_from(Card).filter_by(status=VALID_STATUSES[2])
+            count = db.session.scalar(stmt)
+            # if there is an ongoing card or not
+            if count > 0:
+                raise ValidationError('You already have an ongoing card')
 
     class Meta:
         fields = ('id', 'title', 'description', 'date', 'status', 'priority', 'user', 'comments')
